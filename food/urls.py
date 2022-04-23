18508148d075decf47824404bd32a02c5b96e4fd
Robert <rcoffie22@yahoo.com>
2022-04-23 17:55:16 +0000

    renaming url

--- food/urls.py
+++ food/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = "food"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("", views.item, name="item"),
+    path("item/", views.item, name="item"),
     path("<int:item_id>/", views.detail, name="detail"),
 ]
